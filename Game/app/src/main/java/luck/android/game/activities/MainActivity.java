ebac99f2877583e5db7a32db33bf8b57239244af
JitendraS Singh <JitendraSingh@HANDYGOIT.COM>
2017-02-15 19:46:36 +0530

    minor commit

--- Game/app/src/main/java/luck/android/game/activities/MainActivity.java
+++ Game/app/src/main/java/luck/android/game/activities/MainActivity.java
@@ -1,4 +1,4 @@
-package luck.android.game;
+package luck.android.game.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import luck.android.game.R;
 
 public class MainActivity extends AppCompatActivity {
 
