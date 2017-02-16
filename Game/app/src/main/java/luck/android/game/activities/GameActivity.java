d64c130389e0f4c6ccd3fad835502ab03517de0c
JitendraS Singh <JitendraSingh@HANDYGOIT.COM>
2017-02-16 17:33:22 +0530

    Custom font added

--- Game/app/src/main/java/luck/android/game/activities/GameActivity.java
+++ Game/app/src/main/java/luck/android/game/activities/GameActivity.java
@@ -1,21 +1,35 @@
 package luck.android.game.activities;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import luck.android.game.R;
 
 public class GameActivity extends AppCompatActivity {
 
+    private TextView mFirstPlayer;
+    private TextView mSecondPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dicegame);
+        initializeViews();
+    }
 
+    private void initializeViews()
+    {
+        mFirstPlayer = (TextView)findViewById(R.id.fplayername);
+        mSecondPlayer = (TextView)findViewById(R.id.splayername);
+        Typeface myFont = Typeface.createFromAsset(getAssets(), "fonts/Pacifico.ttf");
+        mFirstPlayer.setTypeface(myFont);
+        mSecondPlayer.setTypeface(myFont);
     }
 
 }
